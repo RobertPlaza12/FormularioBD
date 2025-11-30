8b7a9ae18fcfc92fdfa923d2c0af7f29641805b2
RobertPlaza12 <olivaresrobert1208@gmail.com>
2025-11-30 17:35:30 -0400

    Update inv.js

--- inv.js
+++ inv.js
@@ -1,5 +1,5 @@
 //llamada al backend
-const API = "api/inv.php";
+const API = "https://inv-ferreteriagonzalez.xo.je/api/inv.php";
 
 async function api(action, extra = {}) {
     const res = await fetch(API, {
@@ -136,4 +136,5 @@ async function buscar() {
         alert("No existe un registro con ese ID.");
     }
 }
+
 cargarTabla();
